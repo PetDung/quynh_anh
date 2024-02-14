521b0ec221b13f6529a4400374c44ddfa7e514f3
PetDung <perdhd1702@gmail.com>
2024-02-14 18:14:00 +0700

    fist

---
+++ main.js
@@ -0,0 +1,50 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent = `Cảm ơn tình yêu em dành cho anh, anh đang rất nhớ em nhiều lắm.
+Cuộc sống đã không dịu dàng với em, anh mong những thứ nhỏ bé như này làm em dễ chịu hơn.
+Khoảng cách có thể ngăn cản anh nhìn thấy em, 
+chạm vào em nhưng không thể ngăn được tình yêu anh dành cho em mỗi ngày. 
+Yêu xa là một thử thách lớn nhưng sẽ là một kết thúc tươi đẹp nếu chúng ta vượt qua được.
+ Hãy cùng nhau cố gắng em nhé!
+`
+
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 50 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
